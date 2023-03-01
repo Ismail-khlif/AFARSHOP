e4248a66d1510423271d91857b023aa8ae9637fa
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-03-01 17:40:57 +0100

    service photo et video

--- Back_End_Spring/src/main/java/pidev/afarshop/Controller/Product/ProductController.java
+++ Back_End_Spring/src/main/java/pidev/afarshop/Controller/Product/ProductController.java
@@ -1,5 +1,6 @@
 package pidev.afarshop.Controller.Product;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import pidev.afarshop.Entity.*;
 import pidev.afarshop.Repository.*;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,11 +22,27 @@ public class ProductController {
     ProductRepository productRepository;
 
     @PostMapping(value = "/addandupdateproduct" ,consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Product addProduct(@RequestParam("image") MultipartFile image, @RequestParam("productName") String poductName) throws IOException {
+    public Product addProduct(@RequestParam("image") MultipartFile image, @RequestParam("productName") String poductName,
+                              @RequestParam("reference") String reference,@RequestParam("description") String description,
+                              @RequestParam("quantity") Long quantity,@RequestParam("rating") float rating,
+                              @RequestParam("video") MultipartFile video,@RequestParam("price") float price,
+                              @RequestParam("date")  @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateOfProduct,
+                              @RequestParam("discount") float discount,@RequestParam("brand") String brand,
+                              @RequestParam("yearsOfWarranty") int yearsOfWarranty) throws IOException {
 
         Product product = new Product();
         product.setProductName(poductName);
+        product.setBrand(brand);
         product.setImages(image.getBytes());
+        product.setReference(reference);
+        product.setDescription(description);
+        product.setQuantity(quantity);
+        product.setVideo(video.getBytes());
+        product.setPrice(price);
+        product.setDateOfProduct(dateOfProduct);
+        product.setDiscount(discount);
+        product.setYearsOfWarranty(yearsOfWarranty);
+        product.setRating(rating);
         productRepository.save(product);
         return product;
     }
